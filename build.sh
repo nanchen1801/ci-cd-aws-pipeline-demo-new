814a2a27498d75dc295c77bf114240102c6dda6c
nanchen1801 <nan.chen@sky.uk>
2022-09-03 10:52:58 +0100

    1

--- build.sh
+++ build.sh
@@ -1,2 +1,1 @@
-#!/usr/bin/env bash 
-npm ci,npm run build,npx cdk synth -c config=it
+#!/bin/bash npm ci,npm run build
--- lib/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash 
-npm ci,npm run build,npx cdk synth -c config=it
